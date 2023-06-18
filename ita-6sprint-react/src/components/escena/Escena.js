11c5fc2d3e80e85927717c0acc6c50a11d156358
oscarmoreno222 <oscar.moreno222@gmail.com>
2023-06-18 18:46:23 +0200

    exercici 1

---
+++ ita-6sprint-react/src/components/escena/Escena.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Escena = () => {
+  return (
+    <div>
+      <p>
+        El nostre heroi estava surant per l'espai sideral quan a la llunyania va
+        albirar una nau espacial
+      </p>
+    </div>
+  );
+};
+
+export default Escena;
