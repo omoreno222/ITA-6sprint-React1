c902d0d76aaa27f617b614320072f29259f3b2b0
oscarmoreno222 <oscar.moreno222@gmail.com>
2023-06-18 20:15:31 +0200

    exercici 3

--- ita-6sprint-react/src/App.js
+++ ita-6sprint-react/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import Escena from "./components/escena/Escena";
+import { Radiado } from "./styled";
 import arrayText from "./textHistoria.js";
 
 function App() {
   return (
-    <div>
+    <div className>
       {arrayText.map((item) => (
-        <Escena text={item.text} />
+        <Radiado>
+          <Escena text={item.text} />
+        </Radiado>
       ))}
     </div>
   );
---
+++ ita-6sprint-react/src/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Radiado = styled.div`
+  border: 2px solid black;
+  border-radius: 4rem;
+  margin: 0.5rem 2rem 0.5rem 2rem;
+  text-align: center;
+`;
