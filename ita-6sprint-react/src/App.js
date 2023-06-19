cb2b591a3a4dd77b26b7d9a87cea356de0a334a7
oscarmoreno222 <oscar.moreno222@gmail.com>
2023-06-19 08:59:32 +0200

    Nivel 1 del Sprint

--- ita-6sprint-react/src/App.js
+++ ita-6sprint-react/src/App.js
@@ -1,15 +1,33 @@
 import React from "react";
 import "./App.css";
 import Escena from "./components/escena/Escena";
-import { Radiado } from "./styled";
+import { Boton, Radiado, Contenedor } from "./styled";
 import arrayText from "./textHistoria.js";
+import { useState } from "react";
 
 function App() {
+  const [posicion, setPosicion] = useState(0);
+  const menosPosicion = () => {
+    if (posicion > 0) {
+      setPosicion(posicion - 1);
+    }
+  };
+  const masPosicion = () => {
+    if (posicion < arrayText.length - 1) {
+      setPosicion(posicion + 1);
+    }
+  };
+
   return (
-    <div className>
-      {arrayText.map((item) => (
-        <Radiado>
-          <Escena text={item.text} />
+    <div className="pral">
+      <Contenedor>
+        <Boton onClick={menosPosicion}>Anterior</Boton>
+        <Boton onClick={masPosicion}>Següent</Boton>
+      </Contenedor>
+
+      {arrayText.map((item, index) => (
+        <Radiado active={index === posicion}>
+          <Escena text={item.text} key={item.id} />
         </Radiado>
       ))}
     </div>
--- ita-6sprint-react/src/styled.js
+++ ita-6sprint-react/src/styled.js
@@ -5,4 +5,18 @@ export const Radiado = styled.div`
   border-radius: 4rem;
   margin: 0.5rem 2rem 0.5rem 2rem;
   text-align: center;
+  background-color: ${({ active }) => (active ? "#eeb4b3" : "transparent")};
+  }
+`;
+
+export const Contenedor = styled.div`
+  display: flex;
+`;
+
+export const Boton = styled.button`
+  width: 50vw;
+  padding: 1rem;
+  margin: 0.5rem 2rem 0.5rem 2rem;
+  text-align: center;
+  }
 `;
